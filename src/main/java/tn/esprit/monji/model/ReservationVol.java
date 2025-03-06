7d9e6f2a968c729b224510b60f4c8e238c5d7ca1
mongigi <medmonji.kraoua@esprit.tn>
2025-03-06 12:13:19 +0100

    implementation de l'api aircraft

--- src/main/java/tn/esprit/monji/model/ReservationVol.java
+++ src/main/java/tn/esprit/monji/model/ReservationVol.java
@@ -16,7 +16,7 @@ public class ReservationVol {
         this.idVol = idVol;
         this.classe = classe;
         this.nbBillets = nbBillets;
-    }
+    }     
 
     // Getters and Setters
     public int getId() {
